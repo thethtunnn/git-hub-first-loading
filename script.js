02adca39d4c7e9da80465580cdc802b850657324
thethtunnn <THET HTUN>
2022-10-11 08:57:26 +0900

    san kyi tar bar

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let age = 16;
-if (age < 18){
-    welcome();
-    function welcome() {
-        alert("Helllo");
-    }
-    welcome();
-}else{
-    function welcome() {
-        alert("Greetings");
-    }
-}
-welcome();
+dfdfdffdds
